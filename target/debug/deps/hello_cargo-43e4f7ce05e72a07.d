c7ac1fdc78f3516e06b749d3673b458b652d5870
Ignacio F. Castillejo Gómez <i.castillejogomez@gmail.com>
2022-11-09 16:22:57 +0100

    Hello world with cargo

---
+++ target/debug/deps/hello_cargo-43e4f7ce05e72a07.d
@@ -0,0 +1,5 @@
+/Users/nacho/Code/rust-learning-path/target/debug/deps/hello_cargo-43e4f7ce05e72a07: src/main.rs
+
+/Users/nacho/Code/rust-learning-path/target/debug/deps/hello_cargo-43e4f7ce05e72a07.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/Users/nacho/Code/rust-learning-path/target/debug/hello-cargo: /Users/nacho/Code/rust-learning-path/src/main.rs
