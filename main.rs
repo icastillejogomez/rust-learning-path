feada4ff7994c6525d3781066b7661888b952773
Ignacio F. Castillejo Gómez <i.castillejogomez@gmail.com>
2022-11-09 16:39:21 +0100

    Add println example with string interpolation

--- main.rs
+++ main.rs
@@ -1,3 +1,4 @@
 fn main() {
-  println!("Hola Mundo!!!!");
+    // Call println! with three arguments: a string, a value and a value
+    println!("The first letter of the English alphabet is {} and the last letter is {}.", 'A', 'Z')
 }
